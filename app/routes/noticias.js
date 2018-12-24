f49095262ad1a38a3c3372215bf42801f90c7d88
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-24 03:35:45 -0200

    Fazendo conexao com o banco de portal_noticias e exibindo as noticias cadastradas no banco (adicionado mais um registro na tabela para testes)

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,17 @@
 
 module.exports = function (app){
+    
     app.get('/noticias',function(req,res){
-        res.render("noticias/noticias");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'262517kaka',
+        database:'portal_noticias'
+    });
+    connection.query('SELECT * FROM noticia',function(error,result){
+        res.send(result);
+    });
+        //res.render("noticias/noticias");
     });
 }
--- scripts-banco/carga_dados/insert_noticias.sql
+++ scripts-banco/carga_dados/insert_noticias.sql
@@ -1,3 +1,8 @@
 INSERT INTO portal_noticias.noticia
 (titulo, noticia, data_criacao)
 VALUES('Título da Notícia', 'Conteúdo da Notícia', CURRENT_TIMESTAMP);
+
+INSERT INTO portal_noticias.noticia
+(titulo, noticia, data_criacao)
+VALUES('Título da Notícia 2 ', 'Conteúdo da Notícia 2', CURRENT_TIMESTAMP);
+
