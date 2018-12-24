7c2acbff9aa47f9db03caac3dc96b59f1941ea66
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-24 04:53:47 -0200

    Aula 30. Refactoring do projeto pratico portal de noticias com Consign (routes)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var app = require('./config/server');
 
-require('./app/routes/noticias')(app);
+//require('./app/routes/noticias')(app);
 
-require('./app/routes/home')(app);
+//require('./app/routes/home')(app);
 //Podem ser feito o require de uma função do jeito de cima e de baixo
-var rotaFormularioNoticia = require('./app/routes/formulario-inlusao-noticia');
-rotaFormularioNoticia(app);
+//var rotaFormularioNoticia = require('./app/routes/formulario-inlusao-noticia');
+//rotaFormularioNoticia(app);
 
 app.listen(3000,function(){
     console.log('Servidor Rodando Com Express');    
--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var consign = require('consign');
+
 app.set('view engine','ejs');
-app.set('views','./app/views')
+app.set('views','./app/views');
+consign().include('app/routes').into(app);
 
 module.exports = app;
