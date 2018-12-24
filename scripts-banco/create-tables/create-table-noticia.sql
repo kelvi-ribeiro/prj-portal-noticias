e40deb61dd201146cd43c2fc312c7f1a8bec0b3b
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-24 17:21:00 -0200

    Aula 44. Adicionando mais campos para testes com o express-validator

--- scripts-banco/create-tables/create-table-noticia.sql
+++ scripts-banco/create-tables/create-table-noticia.sql
@@ -2,5 +2,8 @@ CREATE TABLE noticia (
 	id_noticia int NOT NULL PRIMARY key AUTO_INCREMENT,
 	titulo varchar (100),
 	noticia text,
+	resumo varchar(100),
+	autor varchar(40),
+    data_noticia date,
 	data_criacao timestamp default CURRENT_TIMESTAMP
 );
