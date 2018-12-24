2429052d80acf83baba00a9afd591c5598ce6e59
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-24 16:49:57 -0200

    Aula 43. Instalando o express-validator no server.js

--- config/server.js
+++ config/server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 app.set('view engine','ejs');
 app.set('views','./app/views');
 
-app.use(bodyParser.urlencoded({
-        extended:true
-}))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressValidator())
 
 consign()
         .include('app/routes')
