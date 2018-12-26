4660020bb064ec4f615fcbbfd19cd5ec9439b66f
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-26 04:06:51 -0200

    Ajustando time que e inserido data_criacao

--- scripts-banco/carga_dados/insert_noticias.sql
+++ scripts-banco/carga_dados/insert_noticias.sql
@@ -4,21 +4,21 @@ INSERT INTO portal_noticias.noticia
 (titulo, noticia, resumo, autor, data_noticia, data_criacao)
 VALUES('Cristiano Ronaldo marca mais um gol', 
 'Cristiano Ronaldo marca presença no jogo contra Milan, marcando gol de pênalti aos 45 do segundo tempo',
-'Cristiano marca gol no Milan', 'Deivisson Martins','2018-03-01', NOW());
+'Cristiano marca gol no Milan', 'Deivisson Martins','2018-03-01', NOW() - 6);
 
 INSERT INTO portal_noticias.noticia
 (titulo, noticia, resumo, autor, data_noticia, data_criacao)
 VALUES('Game Awards 2018: God of War ganha como jogo do ano',
 'God of War foi eleito o melhor jogo do ano no Game Awards 2018 nesta quinta-feira (6). Para levar a categoria principal, o jogo do Santa Monica Studio superou Celeste, Spider-Man, Monster Hunter: World e Red Dead Redemption 2. Já o game de velho oeste foi o maior ganhador da noite, com quatro prêmios.',
 'Veja lista completa com indicados e vencedores. Evento foi realizado na quinta-feira (6) em Los Angeles.', 
-'Kelvi Martins', '2018-08-01', NOW());
+'Kelvi Martins', '2018-08-01', NOW() - 7);
 
 INSERT INTO portal_noticias.noticia
 (titulo, noticia, resumo, autor, data_noticia, data_criacao)
 VALUES('Indonésia retoma buscas por desaparecidos após tsunami', 
 'O governo da Indonésia retomou nesta quarta-feira (26) os trabalhos de busca das 154 pessoas consideradas desaparecidas após o tsunami que varreu no último sábado o litoral do estreito de Sonda. Segundo o último balanço oficial, o número de mortos chegou a 429. Além disso, mais de 1,5 mil pessoas ficaram feridas pelo tsunami, que deixou mais de 16 mil desalojados.', 
 'Ondas gigantes ainda deixaram 429 mortos e mais de 1.500 feridos.', 
-'Deivisson Martins', '2006-01-11', NOW());
+'Deivisson Martins', '2006-01-11', NOW() - 8);
 
 INSERT INTO portal_noticias.noticia
 (titulo, noticia, resumo, autor, data_noticia, data_criacao)
@@ -26,7 +26,7 @@ VALUES('Só 20% de pobres com acesso a saneamento básico pagam tarifa subsidiad
 'Apenas 20% da população da baixa renda com acesso a água tratada e rede de esgoto no Brasil se beneficiam da chamada tarifa social, subsidiada. É o que aponta estudo divulgado nesta quarta-feira (26) pela Associação Brasileira de Agências de Regulação (ABAR).',
 'Prestadores de serviço não são obrigados a oferecer a chamada tarifa social.', 
 'Thaís Almeida', 
-'2014-12-01', NOW());
+'2014-12-01', NOW() - 9);
 
 INSERT INTO portal_noticias.noticia
 (titulo, noticia, resumo, autor, data_noticia, data_criacao)
@@ -34,5 +34,5 @@ VALUES('Após três dias, rapaz atingido por tromba de água em rapel volta anda
 'Após três dias de buscas, a sexta vítima atingida pela tromba de água em uma cachoeira em São João Batista do Glória (MG) no último sábado (22) apareceu com vida na própria casa, em Passos (MG), no início da noite desta terça-feira (25). A informação foi confirmada pela família do rapaz. Outras cinco pessoas morreram no incidente.', 
 'Eduardo Gomes Moraes estava desaparecido desde a tarde de sábado (22).', 
 'Kauan Martins',
-'2005-06-06', NOW());
+'2005-06-06', NOW() - 10);
 
