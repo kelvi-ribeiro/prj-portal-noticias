7eb4862ea773019070daa6557bb3a6790fcced12
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-26 04:07:26 -0200

    Aula 54. Reaplicando a logica de back-end parte 3

--- app/models/NoticiaDAO.js
+++ app/models/NoticiaDAO.js
@@ -2,11 +2,11 @@
     this._connection = connection;
  }
  NoticiaDAO.prototype.getNoticias = function(callback){
-    this._connection.query('SELECT n.id_noticia,n.titulo FROM noticia as n',callback); 
+    this._connection.query('SELECT n.* FROM noticia AS n ORDER BY n.data_criacao DESC',callback); 
  }
 
  NoticiaDAO.prototype.getNoticia = function(callback){
-    this._connection.query('SELECT n.* FROM noticia as n where n.id_noticia = 1',callback);  
+    this._connection.query('SELECT n.* FROM noticia AS n where n.id_noticia = 1',callback);  
  }
 
  NoticiaDAO.prototype.salvarNoticia = function(noticia,callback){
